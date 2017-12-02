d912ed9da6d7ea1a4c019616b817478bd366be8b
Tomasz Adamski <tadamski@redhat.com>
2017-12-03 00:40:42 +0100

    Only add method

--- src/main/java/org/packt/swarm/petstore/order/OrderResource.java
+++ src/main/java/org/packt/swarm/petstore/order/OrderResource.java
@@ -3,6 +3,7 @@ package org.packt.swarm.petstore.order;
 import org.packt.swarm.petstore.order.model.Order;
 
 import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -28,19 +29,6 @@ public class OrderResource {
     public Response createOrder(@QueryParam("customerId") int customerId, @QueryParam("itemIds") List<Integer> itemIds, @QueryParam("quantities") List<Integer> quantites) {
         try {
             Order order = orderService.createOrder(customerId, itemIds, quantites);
-            return Response.ok(order).build();
-        } catch (Exception e) {
-            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
-        }
-    }
-
-    @POST
-    @Path("order/{id}/payed")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response markAsPayed(@PathParam("id") int orderId) {
-        try {
-            Order order = orderService.changeState(orderId, Order.OrderState.PAID);
             return Response.ok(order).build();
         } catch (Exception e) {
             return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
